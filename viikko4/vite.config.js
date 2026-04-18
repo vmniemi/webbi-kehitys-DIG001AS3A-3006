7b45572cf794860e31b27b07a446044b51b942bc
Veikka Niemi <vegerekka173@gmail.com>
2026-04-19 01:12:58 +0300

    add correct base to vite.config.js

--- viikko4/vite.config.js
+++ viikko4/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/webbi-kehitys-DIG001AS3A-3006/viikko4/",
+  base: "/webbi-kehitys-DIG001AS3A-3006/viikko4-site/",
 });
