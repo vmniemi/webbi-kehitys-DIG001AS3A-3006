8831c19f4a5aae1c041049bf0f3286510e7360e0
Veikka Niemi <vegerekka173@gmail.com>
2026-04-18 23:17:17 +0300

    add link to viikko4

--- viikko4/vite.config.js
+++ viikko4/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/webbi-kehitys-DIG001AS3A-3006/viikko4/",
+});
